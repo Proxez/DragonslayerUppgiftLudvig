07dc4b845402967f3cf22b5df16b66c37d592a34
Viktor Sällström <viktorsallstrom@hotmail.com>
2024-10-10 15:31:09 +0200

    Update classer

--- DragonslayerUppgiftLudvig/Dragon.cs
+++ DragonslayerUppgiftLudvig/Dragon.cs
@@ -7,32 +7,17 @@ using System.Threading.Tasks;
 namespace DragonslayerUppgiftLudvig;
 internal class Dragon
 {
-    int Health { get; set; }
-    int Strength { get; set; }
-    int SpellPower { get; set; }
-    int Level { get; set; }
-    int Armor { get; set; }
+    Dragon FireDragon = new Dragon();
+    Dragon VoidDragon = new Dragon();
+    Dragon FrostDragon = new Dragon();
 
-    void FireDragon()
-    {
-
-    }
-    void VoidDragon()
-    {
-
-    }
-    void FrostDragon()
-    {
-
-    }
+   
+    
+    
+    
+    
+    
 
 
-    void BasicDragonStats()
-    {    
-        Health = 150;
-        Strength = 10;
-        SpellPower = 10;
-        Level = 1;
-        Armor = 10;
-    }
+    
 }
